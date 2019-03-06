0845db96d689eb93f92f870aab7b53a515039136
Steven Thuriot <steven.thuriot@sdworx.com>
2019-03-06 09:14:27 +0100

    Implemented FunctorComparer class

---
+++ FunctorComparer/FunctorComparer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FunctorComparer
+{
+    public sealed class FunctorComparer<T> : IEqualityComparer<T>, IComparer<T>
+    {
+        readonly Comparison<T> _comparison;
+
+        public FunctorComparer(Comparison<T> comparison)
+        {
+            _comparison = comparison ?? throw new ArgumentNullException(nameof(comparison), "Functor Comparison cannot be null.");
+        }
+
+        public int Compare(T x, T y) => _comparison(x, y);
+
+        public bool Equals(T x, T y) => _comparison(x, y) == 0;
+
+        public int GetHashCode(T obj) => obj?.GetHashCode() ?? 0;
+
+        //public static implicit operator FunctorComparer<T>(Comparison<T> value)
+        //    => new FunctorComparer<T>(value);
+    }
+}
