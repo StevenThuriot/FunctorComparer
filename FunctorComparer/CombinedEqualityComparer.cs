b2fcd8107b30702841e2ccc128e1446772ed6ee9
Steven Thuriot <steven.thuriot@sdworx.com>
2019-03-06 09:45:15 +0100

    Added a combined equality comparer

---
+++ FunctorComparer/CombinedEqualityComparer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FunctorComparer
+{
+
+    public class CombinedEqualityComparer<T> : IEqualityComparer<T>
+    {
+        readonly IReadOnlyList<IEqualityComparer<T>> _comparers;
+
+        public CombinedEqualityComparer(IEnumerable<IEqualityComparer<T>> comparers)
+        {
+            if (comparers?.Any() != true)
+                throw new ArgumentNullException(nameof(comparers), "Comparers need to be supplied");
+
+            var allComparers = new List<IEqualityComparer<T>>();
+
+            foreach (var comparer in comparers)
+            {
+                switch (comparer)
+                {
+                    case CombinedEqualityComparer<T> combined:
+                        allComparers.AddRange(combined._comparers);
+                        break;
+
+                    default:
+                        allComparers.Add(comparer);
+                        break;
+                }
+            }
+
+            _comparers = allComparers.AsReadOnly();
+        }
+
+        public CombinedEqualityComparer(params IEqualityComparer<T>[] comparers)
+            : this((IEnumerable<IEqualityComparer<T>>)comparers)
+        {
+
+        }
+
+        public bool Equals(T x, T y)
+        {
+            return _comparers.All(c => c.Equals(x, y));
+        }
+
+        public int GetHashCode(T obj)
+        {
+            unchecked
+            {
+                var hashCode = 974974034;
+
+                foreach (var comparer in _comparers)
+                    hashCode = (hashCode * 397) ^ comparer.GetHashCode(obj);
+
+                return hashCode;
+            }
+        }
+    }
+}
