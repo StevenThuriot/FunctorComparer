0b1129babaebd3f5c423bc118a9e34006406d130
Steven Thuriot <steven.thuriot@sdworx.com>
2019-03-06 09:14:45 +0100

    Added extensions to quickly create a comparer

---
+++ FunctorComparer/FunctorComparerExtenions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FunctorComparer
+{
+    public static class FunctorComparerExtenions
+    {
+        public static FunctorComparer<T> AsComparer<T, TKey>(this Func<T, TKey> keySelector, Comparer<TKey> comparer)
+        {
+            if (comparer == null) comparer = Comparer<TKey>.Default;
+
+            int comparison(T x, T y)
+            {
+                return comparer.Compare(keySelector(x), keySelector(y));
+            }
+
+            return new FunctorComparer<T>(comparison);
+        }
+
+        public static FunctorComparer<T> AsComparer<T, TKey>(this Func<T, TKey> keySelector)
+            => keySelector.AsComparer(null);
+
+        public static FunctorComparer<T> AsComparer<T>(this Comparison<T> comparison)
+            => new FunctorComparer<T>(comparison);
+    }
+}
